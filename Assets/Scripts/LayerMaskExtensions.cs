a78948d9bec7a77979ce1ed90f559a4be00f06f6
NeedsSoySauce <feras.albaroudi@gmail.com>
2022-01-30 03:53:20 +1300

    Place buildings

---
+++ Assets/Scripts/LayerMaskExtensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+ 
+public static class LayerMaskExtensions{
+ 
+    /// http://answers.unity.com/answers/1137700/view.html
+    public static bool Contains(this LayerMask mask, int layer)
+    {
+        return mask == (mask | (1 << layer));
+    }
+}
--- Assets/Scripts/RaycastClick.cs
+++ Assets/Scripts/RaycastClick.cs
@@ -1,16 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
+
+[Serializable]
+public class PlaceableBuilding
+{
+    public GameObject prefab;
+    public int cost;
+}
 
 public class RaycastClick : MonoBehaviour
 {
+    public Sprite invalidBuildingPlacementCursorImage;
     public Sprite hoverSprite;
     public Image cursorImage;
     public TextMeshProUGUI cursorText;
 
-    public LayerMask layerMask;
+    public LayerMask groundMask;
+    public LayerMask invalidBuildingPlacementMask;
+
+    [FormerlySerializedAs("layerMask")] public LayerMask clicklayerMask;
+
+    public int reach = 100;
+
+    public List<PlaceableBuilding> buildings;
+    private GameObject _building;
+
+    private BoxCollider _buildingCollider;
+    private int _buildingIndex;
 
     private Sprite _initialCursorSprite;
+    private bool _isPlacingBuilding;
 
     public void Awake()
     {
@@ -20,20 +44,7 @@ public class RaycastClick : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 100, layerMask))
-        {
-            var building = hit.transform.gameObject.GetComponent<Building>();
-            cursorText.text = building.displayName;
-            cursorImage.sprite = hoverSprite;
-
-            if (Input.GetKeyDown(KeyCode.Mouse0))
-            {
-                building.OpenOverlay();
-            }
-        }
-        else
+        if (!CheckPlaceBuilding() && !CheckClickBuilding())
         {
             cursorText.text = null;
             cursorImage.sprite = _initialCursorSprite;
@@ -43,5 +54,102 @@ public class RaycastClick : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.DrawRay(transform.position, transform.position + transform.forward * 1000);
+    }
+
+    private bool CheckPlaceBuilding()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1)) _isPlacingBuilding = !_isPlacingBuilding;
+
+        if (_isPlacingBuilding)
+        {
+            var isCtrlDown = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+            var mouseScroll = Input.GetAxisRaw("Mouse ScrollWheel");
+
+            var isBuildingChanged = false;
+
+            if (isCtrlDown && _building)
+            {
+                if (mouseScroll > 0.1)
+                    _building.transform.Rotate(Vector3.up, 5);
+                else if (mouseScroll < -0.1) _building.transform.Rotate(Vector3.up, -5);
+            }
+            else if (!isCtrlDown)
+            {
+                if (mouseScroll > 0.1)
+                {
+                    _buildingIndex += 1;
+                    isBuildingChanged = true;
+                    if (_buildingIndex == buildings.Count()) _buildingIndex = 0;
+                }
+                else if (mouseScroll < -0.1)
+                {
+                    _buildingIndex -= 1;
+                    isBuildingChanged = true;
+                    if (_buildingIndex < 0) _buildingIndex = buildings.Count - 1;
+                }
+            }
+
+            var placeableBuilding = buildings[_buildingIndex];
+
+
+            var hasGroundHit = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),
+                out var groundHit, reach, groundMask);
+
+            if (isBuildingChanged || _building == null)
+            {
+                if (_building != null) Destroy(_building);
+                _building = Instantiate(placeableBuilding.prefab, Vector3.down * 1000, Quaternion.identity);
+                _buildingCollider = _building.GetComponent<BoxCollider>();
+                _buildingCollider.enabled = false;
+            }
+
+            if (hasGroundHit) _building.transform.position = groundHit.point;
+
+            if (!hasGroundHit || Physics.CheckBox(groundHit.point, _buildingCollider.size / 2, Quaternion.identity,
+                invalidBuildingPlacementMask))
+            {
+                cursorText.text = "You can't build here.";
+                cursorImage.sprite = invalidBuildingPlacementCursorImage;
+                return true;
+            }
+
+            // Valid building placement
+            cursorText.text = @"Left click to place building.
+
+Mouse wheel to change building.
+
+CTRL + Mouse Wheel to rotate.";
+
+            if (!Input.GetKeyDown(KeyCode.Mouse0)) return true;
+
+            // Place building
+            _buildingCollider.enabled = true;
+            _buildingCollider = null;
+            _building = null;
+
+            Debug.Log("Place building");
+            return true;
+        }
+
+        if (_building != null) Destroy(_building);
+
+        return false;
+    }
+
+    private bool CheckClickBuilding()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out var hit, reach,
+            clicklayerMask))
+        {
+            var building = hit.transform.gameObject.GetComponent<Building>();
+            cursorText.text = building.displayName;
+            cursorImage.sprite = hoverSprite;
+
+            if (Input.GetKeyDown(KeyCode.Mouse0)) building.OpenOverlay();
+
+            return true;
+        }
+
+        return false;
     }
 }
